1e4bf5c5e5e69a34c59c9c1f43f09dcc1d5dc54c
manjuprem12 <manjupremp@gmail.com>
2019-06-14 15:59:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h2> TASK </h2>
+      </div>
+    )
+  }
+}
+export default App;
